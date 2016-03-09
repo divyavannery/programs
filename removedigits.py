b6696765efbe5ab4093debb7d835c9349e5f057b
divya vannery <divyavannery@yahoo.com>
2016-03-09 12:25:01 +0530

    error corrected in removedigits

--- removedigits.py
+++ removedigits.py
@@ -9,13 +9,15 @@ print comb
 combinations=list(itertools.combinations(comb,k))
 num=number
 array=[]
-print combinations
+combinations=sorted(combinations)
 for i in combinations:
     num=number
+    count=0
     for j in i:
         j=int(j)
+        j=j-count
         num=num[:j]+num[(j+1):]
+        count=count+1
     array.append(int(num))
-    print int(num)
 array.sort()
 print "ans",array[0]
